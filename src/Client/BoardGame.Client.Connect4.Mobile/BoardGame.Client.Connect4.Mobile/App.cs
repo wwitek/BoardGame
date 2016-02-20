ea1b5d22cf55dfc7215055bf7e71b55bb77dddc8
Wojciech Witek <wojtekwitek@gmail>
2016-02-20 21:22:26 +0100

    Added dependencies and views

--- src/Client/BoardGame.Client.Connect4.Mobile/BoardGame.Client.Connect4.Mobile/App.cs
+++ src/Client/BoardGame.Client.Connect4.Mobile/BoardGame.Client.Connect4.Mobile/App.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BoardGame.Client.Connect4.Mobile.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,19 +13,7 @@ namespace BoardGame.Client.Connect4.Mobile
         public App()
         {
             // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new StartPage();
         }
 
         protected override void OnStart()
---
+++ src/Client/BoardGame.Client.Connect4.Mobile/BoardGame.Client.Connect4.Mobile/Views/SinglePlayerPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace BoardGame.Client.Connect4.Mobile.Views
+{
+    public partial class SinglePlayerPage : ContentPage
+    {
+        public SinglePlayerPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
